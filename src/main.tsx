86a5198664fb473f2ddcc92b183d16ff5dd79e68
correcaminos <orangeberet@gmail.com>
2023-04-12 12:30:41 +0200

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import ReactDOM from 'react-dom';
 import App from './App';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
@@ -9,6 +8,9 @@ import { defineCustomElements } from '@ionic/pwa-elements/loader';
 defineCustomElements(window);
 const container = document.getElementById('root');
 const root = createRoot(container!);
-ReactDOM.render(<App />, document.getElementById('root'));
-
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 // Learn more about service workers: https://cra.link/PWA
